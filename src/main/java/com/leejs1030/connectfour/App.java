370e43a2a8f391de9975410cb61d6449d124ab6e
leejs1030 <leejs1030@korea.ac.kr>
2022-01-29 23:35:52 +0900

    Feat: Add App.java that have main method

--- src/main/java/com/leejs1030/connectfour/App.java
+++ src/main/java/com/leejs1030/connectfour/App.java
@@ -1,10 +1,13 @@
 package com.leejs1030.connectfour;
 
+import com.leejs1030.connectfour.game.Game;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Game game = new Game();
+        int winner = game.playGame();
+        System.out.println("Player " + winner + "의 승리!");
     }
 }
